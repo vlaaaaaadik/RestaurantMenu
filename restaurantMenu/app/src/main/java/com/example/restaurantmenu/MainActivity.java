6f8531e75631d4b2bb770a39506ff1b878f35d59
Влад Мотрунич <onistarvlaad@gmail.com>
2021-01-30 19:25:48 +0200

    added sign up activity, fix squares and button on main activity

--- restaurantMenu/app/src/main/java/com/example/restaurantmenu/MainActivity.java
+++ restaurantMenu/app/src/main/java/com/example/restaurantmenu/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.restaurantmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final MainActivity context = this;
+        findViewById(R.id.signUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
